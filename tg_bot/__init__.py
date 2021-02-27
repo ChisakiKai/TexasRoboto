3cc531a8e0f71bca6111655b1ac48dc3120475a9
Restyled.io <commits@restyled.io>
2021-02-27 17:52:55 +0000

    Restyled by isort

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -6,9 +6,9 @@ import telegram.ext as tg
 from loguru import logger
 from pyrogram import Client
 
-from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler
-from tg_bot.modules.helper_funcs.handlers import CustomMessageHandler
-from tg_bot.modules.helper_funcs.handlers import CustomRegexHandler
+from tg_bot.modules.helper_funcs.handlers import (CustomCommandHandler,
+                                                  CustomMessageHandler,
+                                                  CustomRegexHandler)
 
 
 class InterceptHandler(logging.Handler):
