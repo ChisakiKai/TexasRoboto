66d8ebd6b71c1d03198304a6ee2d95e54c28cc00
ChisakiKai <anomali@qmail.id>
2021-02-28 18:12:51 +0700

    maybe fix
    Signed-off-by: ChisakiKai <anomali@qmail.id>

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -1,186 +1,155 @@
 import logging
 import os
 import sys
-
+import time
+import spamwatch
 import telegram.ext as tg
-from loguru import logger
-from pyrogram import Client
-
-class InterceptHandler(logging.Handler):
-    LEVELS_MAP = {
-        logging.CRITICAL: "CRITICAL",
-        logging.ERROR: "ERROR",
-        logging.WARNING: "WARNING",
-        logging.INFO: "INFO",
-        logging.DEBUG: "DEBUG"
-    }
-
-    def _get_level(self, record):
-        return self.LEVELS_MAP.get(record.levelno, record.levelno)
-
-    def emit(self, record):
-        logger_opt = logger.opt(depth=6, exception=record.exc_info, ansi=True, lazy=True)
-        logger_opt.log(self._get_level(record), record.getMessage())
-
-
-logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
+from telethon import TelegramClient
+from pyrogram import Client, errors
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
+from pyrogram.types import Chat, User
+from configparser import ConfigParser
+from rich.logging import RichHandler
+StartTime = time.time()
 
 # enable logging
-LOGGER = logging.getLogger(__name__)
+FORMAT = "%(message)s"
+logging.basicConfig(handlers=[RichHandler()], level=logging.INFO, format=FORMAT, datefmt="[%X]")
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+log = logging.getLogger("rich")
+
+log.info("Twxas is starting. | An Zero Union Project. | Licensed under GPLv3.")
+
+log.info("Not affiliated to Ark or Levels in any way whatsoever.")
+log.info("Project maintained by: github.com/ChisakiKai (t.me/ChisakiKai/TexasRoboto)")
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
-    LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
+    log.error(
+        "You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting."
+    )
     quit(1)
 
-ENV = bool(os.environ.get('ENV', False))
+parser = ConfigParser()
+parser.read("config.ini")
+kagconfig = parser["kagconfig"]
 
-if ENV:
-    TOKEN = os.environ.get('TOKEN', None)
-    try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', None))
-    except ValueError:
-        raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-    MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+OWNER_ID = kagconfig.getint("OWNER_ID")
+OWNER_USERNAME = kagconfig.get("OWNER_USERNAME")
+APP_ID = kagconfig.getint("APP_ID")
+API_HASH = kagconfig.get("API_HASH")
+WEBHOOK = kagconfig.getboolean("WEBHOOK", False)
+URL = kagconfig.get("URL", None)
+CERT_PATH = kagconfig.get("CERT_PATH", None)
+PORT = kagconfig.getint("PORT", None)
+INFOPIC = kagconfig.getboolean("INFOPIC", False)
+DEL_CMDS = kagconfig.getboolean("DEL_CMDS", False)
+STRICT_GBAN = kagconfig.getboolean("STRICT_GBAN", False)
+ALLOW_EXCL = kagconfig.getboolean("ALLOW_EXCL", False)
+CUSTOM_CMD = kagconfig.get("CUSTOM_CMD", None)
+BAN_STICKER = kagconfig.get("BAN_STICKER", None)
+TOKEN = kagconfig.get("TOKEN")
+DB_URI = kagconfig.get("SQLALCHEMY_DATABASE_URI")
+LOAD = kagconfig.get("LOAD").split()
+LOAD = list(map(str, LOAD))
+MESSAGE_DUMP = kagconfig.getfloat("MESSAGE_DUMP")
+GBAN_LOGS = kagconfig.getfloat("GBAN_LOGS")
+NO_LOAD = kagconfig.get("NO_LOAD").split()
+NO_LOAD = list(map(str, NO_LOAD))
+SUDO_USERS = kagconfig.get("SUDO_USERS").split()
+SUDO_USERS = list(map(int, SUDO_USERS))
+DEV_USERS = kagconfig.get("DEV_USERS").split()
+DEV_USERS = list(map(int, DEV_USERS))
+SUPPORT_USERS = kagconfig.get("SUPPORT_USERS").split()
+SUPPORT_USERS = list(map(int, SUPPORT_USERS))
+SARDEGNA_USERS = kagconfig.get("SARDEGNA_USERS").split()
+SARDEGNA_USERS = list(map(int, SARDEGNA_USERS))
+WHITELIST_USERS = kagconfig.get("WHITELIST_USERS").split()
+WHITELIST_USERS = list(map(int, WHITELIST_USERS))
+SPAMMERS = kagconfig.get("SPAMMERS").split()
+SPAMMERS = list(map(int, SPAMMERS))
+spamwatch_api = kagconfig.get("spamwatch_api")
+CASH_API_KEY = kagconfig.get("CASH_API_KEY")
+TIME_API_KEY = kagconfig.get("TIME_API_KEY")
+WALL_API = kagconfig.get("WALL_API")
+LASTFM_API_KEY = kagconfig.get("LASTFM_API_KEY")
+try:
+    CF_API_KEY = kagconfig.get("CF_API_KEY")
+    log.info("AI antispam powered by Intellivoid.")
+except:
+    log.info("No Coffeehouse API key provided.")
+    CF_API_KEY = None
 
-    try:
-        SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
 
-    try:
-        SUPPORT_USERS = set(int(x) for x in os.environ.get("SUPPORT_USERS", "").split())
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
+SUDO_USERS.append(OWNER_ID)
+DEV_USERS.append(OWNER_ID)
 
-    try:
-        WHITELIST_USERS = set(int(x) for x in os.environ.get("WHITELIST_USERS", "").split())
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
-    except ValueError:
-        raise Exception("Your developer users list does not contain valid integers.")
-
-    WEBHOOK = bool(os.environ.get('WEBHOOK', False))
-    URL = os.environ.get('URL', "")  # Does not contain token
-    PORT = int(os.environ.get('PORT', 5000))
-    CERT_PATH = os.environ.get("CERT_PATH")
-
-    DB_URI = os.environ.get('DATABASE_URL')
-    DONATION_LINK = os.environ.get('DONATION_LINK')
-    LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
-    STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
-    WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
-    ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    LASTFM_API_KEY = os.environ.get('LASTFM_API_KEY', "")
-    WALL_API = os.environ.get('WALL_API', "")
-    MOE_API = os.environ.get('MOE_API', "")
-    AI_API_KEY = os.environ.get('AI_API_KEY', "")
-    MAL_CLIENT_ID = os.environ.get('MAL_CLIENT_ID', "")
-    MAL_ACCESS_TOKEN = os.environ.get('MAL_ACCESS_TOKEN', "")
-    MAL_REFRESH_TOKEN = os.environ.get('MAL_REFRESH_TOKEN', "")
-    API_ID = os.environ.get('API_ID', "")
-    API_HASH = os.environ.get('API_HASH', "")
-    try:
-        BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
-    except ValueError:
-        raise Exception("Your blacklisted chats list does not contain valid integers.")
-
+# SpamWatch
+if spamwatch_api is None:
+    sw = None
+    log.warning("SpamWatch API key is missing! Check your config.ini")
 else:
-    from tg_bot.config import Development as Config
-    TOKEN = Config.API_KEY
     try:
-        OWNER_ID = int(Config.OWNER_ID)
-    except ValueError:
-        raise Exception("Your OWNER_ID variable is not a valid integer.")
+        sw = spamwatch.Client(spamwatch_api)
+    except:
+        sw = None
+        log.warning("Can't connect to SpamWatch!")
 
-    MESSAGE_DUMP = Config.MESSAGE_DUMP
-    OWNER_USERNAME = Config.OWNER_USERNAME
-
-    try:
-        SUDO_USERS = set(int(x) for x in Config.SUDO_USERS or [])
-    except ValueError:
-        raise Exception("Your sudo users list does not contain valid integers.")
-
-    try:
-        SUPPORT_USERS = set(int(x) for x in Config.SUPPORT_USERS or [])
-    except ValueError:
-        raise Exception("Your support users list does not contain valid integers.")
-
-    try:
-        WHITELIST_USERS = set(int(x) for x in Config.WHITELIST_USERS or [])
-    except ValueError:
-        raise Exception("Your whitelisted users list does not contain valid integers.")
-
-    try:
-        DEV_USERS = set(int(x) for x in Config.DEV_USERS or [])
-    except ValueError:
-        raise Exception("Your developer users list does not contain valid integers.")
-
-    WEBHOOK = Config.WEBHOOK
-    URL = Config.URL
-    PORT = Config.PORT
-    CERT_PATH = Config.CERT_PATH
-
-    DB_URI = Config.SQLALCHEMY_DATABASE_URI
-    DONATION_LINK = Config.DONATION_LINK
-    LOAD = Config.LOAD
-    NO_LOAD = Config.NO_LOAD
-    DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
-    WORKERS = Config.WORKERS
-    BAN_STICKER = Config.BAN_STICKER
-    ALLOW_EXCL = Config.ALLOW_EXCL
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    WALL_API = Config.WALL_API
-    MOE_API = Config.MOE_API
-    AI_API_KEY = Config.AI_API_KEY
-    MAL_CLIENT_ID = Config.MAL_CLIENT_ID
-    MAL_ACCESS_TOKEN = Config.MAL_ACCESS_TOKEN
-    MAL_REFRESH_TOKEN = Config.MAL_REFRESH_TOKEN
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    try:
-        BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
-    except ValueError:
-        raise Exception ("Your blacklisted chats list does not contain valid integers.")
-
-
-SUDO_USERS.add(OWNER_ID)
-DEV_USERS.add(OWNER_ID)
-
-updater = tg.Updater(TOKEN, workers=WORKERS)
-
+updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
+telethn = TelegramClient("texas", APP_ID, API_HASH)
 dispatcher = updater.dispatcher
 
-SUDO_USERS = list(SUDO_USERS)
+kp = Client("KaguyaPyro", api_id=APP_ID, api_hash=API_HASH, bot_token=TOKEN, workers=min(32, os.cpu_count() + 4))
+apps = []
+apps.append(kp)
+
+
+async def get_entity(client, entity):
+    entity_client = client
+    if not isinstance(entity, Chat):
+        try:
+            entity = int(entity)
+        except ValueError:
+            pass
+        except TypeError:
+            entity = entity.id
+        try:
+            entity = await client.get_chat(entity)
+        except (PeerIdInvalid, ChannelInvalid):
+            for kp in apps:
+                if kp != client:
+                    try:
+                        entity = await kp.get_chat(entity)
+                    except (PeerIdInvalid, ChannelInvalid):
+                        pass
+                    else:
+                        entity_client = kp
+                        break
+            else:
+                entity = await kp.get_chat(entity)
+                entity_client = kp
+    return entity, entity_client
+
+
+SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
+DEV_USERS = list(DEV_USERS)
 WHITELIST_USERS = list(WHITELIST_USERS)
 SUPPORT_USERS = list(SUPPORT_USERS)
+SARDEGNA_USERS = list(SARDEGNA_USERS)
+SPAMMERS = list(SPAMMERS)
 
-# Load at end tsure all prev variables have been set
-from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
+# Load at end to ensure all prev variables have been set
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler
 
-# make sure the regex handler can take extra kwargs
-tg.RegexHandler = CustomRegexHandler
-
-# Exempt blacklisted users from MessageHandler
-tg.MessageHandler = CustomMessageHandler
-
-if ALLOW_EXCL:
+if CUSTOM_CMD and len(CUSTOM_CMD) >= 1:
     tg.CommandHandler = CustomCommandHandler
 
 
-# Pyrogram Client
-pg = Client(
-        "Texas",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=TOKEN
-    )
+def spamfilters(text, user_id, chat_id):
+    # print("{} | {} | {}".format(text, user_id, chat_id))
+    if int(user_id) in SPAMMERS:
+        print("This user is a spammer!")
+        return True
+    else:
+        return False
